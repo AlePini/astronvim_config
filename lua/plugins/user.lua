ce011423bb4ea18cde8c2d7a6305abdc5ad0d792
AlePini <ale@alepini.com>
2025-08-21 16:48:34 +0200

    fix: add heirline config

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -43,6 +43,35 @@ return {
       },
     },
   },
+  {
+    "rebelot/heirline.nvim",
+    opts = function(_, opts)
+      local status = require("astroui.status")
+
+      opts.statusline = { -- statusline
+        hl = { fg = "fg", bg = "bg" },
+        status.component.mode(),
+        status.component.git_branch(),
+        status.component.separated_path({
+          path_func = status.provider.filename({ modify = ":~:h" }),
+        }),
+        status.component.file_info({
+          filename = {},
+          filetype = false,
+        }),
+        status.component.git_diff(),
+        status.component.diagnostics(),
+        status.component.fill(),
+        status.component.cmd_info(),
+        status.component.fill(),
+        status.component.lsp(),
+        status.component.virtual_env(),
+        status.component.treesitter(),
+        status.component.nav(),
+        status.component.mode({ surround = { separator = "right" } }),
+      }
+    end,
+  }
 
   -- == Examples of Overriding Plugins ==
 
