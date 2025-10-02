6fcc62f362116fa0a459a0f4613bc82b4ee8caf8
AlePini <ale@alepini.com>
2025-10-02 15:09:57 +0200

    fix: disable discord presence plugin
    useless

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -26,6 +26,9 @@ return {
   --     require("telescope").load_extension("yaml_schema")
   --   end,
   -- },
+
+  -- == Disable Discord Presence ==
+  { "andweeb/presence.nvim", enabled = false },
   {
     'MeanderingProgrammer/render-markdown.nvim',
     -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
